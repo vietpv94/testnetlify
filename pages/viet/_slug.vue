63cfc25d505d08ad5c1611f3519507e5a93b6c8a
Viet Pham <vietpv94@gmail.com>
2021-03-12 17:57:10 +0700

    Initial Commit

--- pages/viet/_slug.vue
+++ pages/viet/_slug.vue
@@ -10,7 +10,7 @@ export default {
   async asyncData({ $content, params, error }) {
     let post;
     try {
-      post = await $content("blog", params.slug).fetch();
+      post = await $content("viet", params.slug).fetch();
       // OR const article = await $content(`articles/${params.slug}`).fetch()
     } catch (e) {
       error({ message: "Blog Post not found" });
